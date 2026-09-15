ad7212253280177afb94541eba553232919b29ff
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2026-09-15 10:57:18 -0300

    fix: detecção de login redirecionando para validation ao invés de home

--- code/session.js
+++ code/session.js
@@ -7,19 +7,17 @@ if (!isLogged && !window.location.href.includes("/login/login.html")) {
     url = `http://${window.location.host}/code/login/login.html`;
   }
   window.location.href = url;
-} else if (window.location.href.includes("/login/login.html")) {
-  if (isLogged) {
-    const params = new URLSearchParams(window.location.search);
-    const isToLogoff = params.get("logoff");
-    if (isToLogoff) {
-      localStorage.removeItem("isLogged");
-    }
-    let url;
-    if (window.location.protocol.includes("https")) {
-      url = `https://${window.location.host}/pi-1-senac-tecnico-informatica-internet-2026/code/internal/validation/validation.html`;
-    } else {
-      url = `http://${window.location.host}/code/internal/home/home.html`;
-    }
-    window.location.href = url;
+} else if (isLogged && !window.location.href.includes("/internal")) {
+  const params = new URLSearchParams(window.location.search);
+  const isToLogoff = params.get("logoff");
+  if (isToLogoff) {
+    localStorage.removeItem("isLogged");
   }
+  let url;
+  if (window.location.protocol.includes("https")) {
+    url = `https://${window.location.host}/pi-1-senac-tecnico-informatica-internet-2026/code/internal/home/home.html`;
+  } else {
+    url = `http://${window.location.host}/code/internal/home/home.html`;
+  }
+  window.location.href = url;
 }
