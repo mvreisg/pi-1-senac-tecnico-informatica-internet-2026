4a4c242f93ec164c4187eb5bdea32c8ddc1009ab
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2026-09-17 09:26:59 -0300

    feat: adiciona landing page

--- code/session.js
+++ code/session.js
@@ -1,23 +1,27 @@
 const isLogged = localStorage.getItem("isLogged");
-if (!isLogged && !window.location.href.includes("/login/login.html")) {
-  let url;
+let url;
+if (
+  !isLogged &&
+  window.location.pathname.includes("/internal") &&
+  !window.location.pathname.includes("/internal/validation") &&
+  !window.location.pathname.includes("/internal/qrcode")
+) {
   if (window.location.protocol.includes("https")) {
     url = `https://${window.location.host}/pi-1-senac-tecnico-informatica-internet-2026/code/login/login.html`;
   } else {
     url = `http://${window.location.host}/code/login/login.html`;
   }
-  window.location.href = url;
-} else if (isLogged && !window.location.href.includes("/internal")) {
+  navigation.navigate(url);
+} else if (isLogged && !window.location.pathname.includes("/internal")) {
   const params = new URLSearchParams(window.location.search);
   const isToLogoff = params.get("logoff");
   if (isToLogoff) {
     localStorage.removeItem("isLogged");
   }
-  let url;
   if (window.location.protocol.includes("https")) {
     url = `https://${window.location.host}/pi-1-senac-tecnico-informatica-internet-2026/code/internal/home/home.html`;
   } else {
     url = `http://${window.location.host}/code/internal/home/home.html`;
   }
-  window.location.href = url;
+  navigation.navigate(url);
 }
