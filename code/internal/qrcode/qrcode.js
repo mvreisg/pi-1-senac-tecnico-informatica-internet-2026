b9b426905b94344cebf7f2434b2b321f68362ff7
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2026-09-21 10:38:15 -0300

    feat: adiciona responsividade na página de qrcode

--- code/internal/qrcode/qrcode.js
+++ code/internal/qrcode/qrcode.js
@@ -1,12 +1,24 @@
+let url;
+if (window.location.protocol.includes("https")) {
+  url = `https://${window.location.host}/pi-1-senac-tecnico-informatica-internet-2026/code/internal/validation/validation.html`;
+} else {
+  url = `http://${window.location.host}/code/internal/validation/validation.html`;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  let url;
-  if (window.location.protocol.includes("https")) {
-    url = `https://${window.location.host}/pi-1-senac-tecnico-informatica-internet-2026/code/internal/validation/validation.html`;
-  } else {
-    url = `http://${window.location.host}/code/internal/validation/validation.html`;
+  new QRCode(document.getElementById("qrcode"), {
+    width: window.innerWidth / 2,
+    height: window.innerWidth / 2,
+  }).makeCode(url);
+});
+
+window.addEventListener("resize", () => {
+  const element = document.getElementById("qrcode");
+  for (const e of [...element.children]) {
+    e.remove();
   }
   new QRCode(document.getElementById("qrcode"), {
-    width: 400,
-    height: 400,
+    width: window.innerWidth / 2,
+    height: window.innerWidth / 2,
   }).makeCode(url);
 });
